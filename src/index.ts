d3c626d31b1112011b26f228c57b9a656213bc20
Giovanny Garzón Soto <giovannygarzonsoto@gmail.com>
2024-03-19 01:34:15 -0300

    https added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import app from './app'
+import https from 'https'
+import fs from 'fs'
 import {connectDatabase} from './database'
 
 const main = () => {
-    app.listen(app.get('port'))
+    https.createServer({
+        cert: fs.readFileSync('certificate.crt'),
+        key:  fs.readFileSync('secret.key'),
+    }, app).listen(app.get('port'))
+    
     console.log(`Server running on port ${app.get('port')}`)
     connectDatabase()
 }
